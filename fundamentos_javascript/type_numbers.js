f0d86bb9cd4bbf28e50be1d174917870adb63cc2
Giossaurus <gihhskt@gmail.com>
2023-09-03 19:58:49 -0300

    Update type_numbers.js

--- fundamentos_javascript/type_numbers.js
+++ fundamentos_javascript/type_numbers.js
@@ -1,5 +1,4 @@
 //tipo Number
-
 const meuNumero = 3;
 //variavel meuNumero é um espaço na memória que guarda um dado específico e esse dado é do tipo número e com valor 3
 
@@ -7,5 +6,18 @@ const primeiroNumero = 1;
 const segundoNumero = 2;
 
 const operacaoMatematica = primeiroNumero + segundoNumero;
+// Outros operadores para utilizar é - ou / para subtrair e dividir respectivamente
 
 console.log(operacaoMatematica)
+
+// Ponto flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+//NaN -> Not A Number 
+const alura = "Alura";
+console.log(alura * primeiroNumero)
