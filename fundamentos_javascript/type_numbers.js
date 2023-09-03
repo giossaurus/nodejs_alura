7eac3a88b54a6567d4c1a3c0211e4f4f172031f3
Giossaurus <gihhskt@gmail.com>
2023-09-03 19:48:40 -0300

    Revisão conteúdo Javascript - Números
    Anotações de tipos no Javascript

---
+++ fundamentos_javascript/type_numbers.js
@@ -0,0 +1,11 @@
+//tipo Number
+
+const meuNumero = 3;
+//variavel meuNumero é um espaço na memória que guarda um dado específico e esse dado é do tipo número e com valor 3
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
