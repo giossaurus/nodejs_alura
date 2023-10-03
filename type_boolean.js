b49f94742b2114ab69c5f798f5ab477dd5f3a054
Giovanni Della Dea <giossaurus@MacBook-Air-de-Giovanni.local>
2023-10-03 19:44:54 -0300

    exemplos da aula 01

---
+++ type_boolean.js
@@ -0,0 +1,16 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = false;
+
+console.log(primeiroNumero === segundoNumero);
+
+//Um igual apenas no JS é atribuir um valor a variável, dois ou três iguais são comparações, retorna true ou false.
+
+const texto1 = "Alura";
+const texto2 = "A";
+
+console.log(texto1 === texto2);
