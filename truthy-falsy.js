cd8b4d642f8a39805c3d12d8dad7de9c4967b18d
Giovanni Della Dea <giossaurus@MacBook-Air-de-Giovanni.local>
2023-10-09 18:21:50 -0300

    truthy e falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,26 @@
+//Boolean
+const usuarioLogadotrue = true;
+const contaPaga = false;
+
+//Truthy ou falsy
+// 0 = false
+// 1 = true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// null => representa o vazio 
+// undefinied => variável declarada mas sem valor
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
