0bbfeb92e5a7edc66383a65a1c0e4f6f7951c126
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2024-07-16 20:13:34 -0300

    md atualizado

---
+++ javasciript_utilizando_tipos_variaveis_funcoes/boolean.js
@@ -0,0 +1,14 @@
+const estudante = "Pedro";
+const estaAprovado = true;
+
+if (estaAprovado === true) {
+    console.log(`${estudante} está aprovado! parabéns boas festas!`);
+} else {
+    console.log(`${estudante} não está aprovado!`);
+}
+
+if (estudante === 'Fernando') {
+    console.log(`O estudante é o Fernando`);
+} else {
+    console.log(`Ele não é o Fernando, quem é ele?`);
+}
--- javasciript_utilizando_tipos_variaveis_funcoes/strings.js
+++ javasciript_utilizando_tipos_variaveis_funcoes/strings.js
@@ -5,3 +5,18 @@ const citacao = 'Ju diz: "nosso lema é estudar bastante!"';
 
 console.log(cumprimento);
 console.log(citacao);
+
+//template string
+console.log(`${estudante} e ${docente} são colegas de trabalho e ${cumprimento}`);
+
+//conversao de codigo Unicode
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
