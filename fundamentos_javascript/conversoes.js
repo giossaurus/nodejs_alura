b22045139d2fb87b418cb33b40a1619b535b3365
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2024-07-16 19:52:15 -0300

    ex novos update

--- fundamentos_javascript/conversoes.js
+++ fundamentos_javascript/conversoes.js
@@ -8,8 +8,7 @@ const numeroString = "456";
 console.log(numero == numeroString);
 // Com === comparam todos os valores, já == comparam os valores dentro do ""
 
+//Conversão explícita
 //Number()
 //String()
 console.log(numero + Number(numeroString));
-
-//Conversão explícita
