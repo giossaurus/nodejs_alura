f51bdbe63f89570b8e004e7597d40453e9eff121
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2024-07-20 19:30:26 -0300

    exercicios novos

--- javasciript_utilizando_tipos_variaveis_funcoes/aulas/operadores-logicos.js
+++ javasciript_utilizando_tipos_variaveis_funcoes/aulas/operadores-logicos.js
@@ -1,5 +1,6 @@
 const notaFinal = 6;
 const faltas = 5;
+const advertencia = 0;
 
 if (notaFinal < 7 && faltas > 4){
     console.log('Reprovado');
@@ -8,10 +9,15 @@ if (notaFinal < 7 && faltas > 4){
 }
 
 // Abaixo está um exemplo de uso do operador lógico OU (||) para verificar se o aluno foi aprovado ou reprovado com base na nota final e no número de faltas:
-/*
+
 if (notaFinal < 7 || faltas > 4){
     console.log('Reprovado');
 } else{
     console.log('Aprovado');
 }
-*/
+
+if(faltas >= 2 && !advertencia){
+    console.log('Bônus de 2 pontos na nota final');
+}else{
+    console.log('Sem bônus');
+}
